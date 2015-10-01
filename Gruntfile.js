b51d2db409bd6508875a4aa50070deb040717858
Felipe Norato Lacerda <fnoratol@gmail.com>
2015-10-01 16:28:14 -0300

    Create Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+  
+  grunt.initConfig({
+    copy: {
+      project: {
+        expand: true,
+        cwd: '.',
+        src: ['**', '!Gruntfile.js', '!package.json', '!bower.json'],
+        dest: 'dist'
+      }
+    },
+    clean: {
+      dist: {
+        src: 'dist'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('dist', ['clean', 'copy']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+}
