8ec2af0e460c4da59b76efc529f112ab87f896b1
Felipe Norato Lacerda <fnoratol@gmail.com>
2015-10-01 19:50:27 -0300

    Some mods

--- src/controllers/tictactoeCtrl.js
+++ src/controllers/tictactoeCtrl.js
@@ -4,8 +4,8 @@ APP.angular.controller('tictactoeCtrl',
 function tictactoeCtrl ($scope) {
   $scope.board = new APP.entities.Board();
 
-  var iconX = "fa fa-times fa-5x";
-  var iconO = "fa fa-genderless fa-5x";
+  var iconX = "fa fa-times fa-5x red";
+  var iconO = "fa fa-genderless fa-5x blue";
   var pogVar = true;
 
   $scope.click = function (cell) {
