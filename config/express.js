b3e3110d7abca566be493bb7589f0db2a3efd17e
Felipe Norato Lacerda <fnoratol@gmail.com>
2015-10-01 17:01:37 -0300

    Fix bug

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = function () {
   var app = express();
-  var port = Number(process.env.port || 3000);
+  var port = Number(process.env.PORT || 3000);
   app.set('port', port);
   app.use(express.static('./src'));
   return app;
